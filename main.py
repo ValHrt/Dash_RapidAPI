9e3fab0a620523305d3b22a4ef4b72d03f0140a0
Valentin Henriot <59604857+ValHrt@users.noreply.github.com>
2022-12-14 12:26:24 +0100

    Add primary Dash components

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from dash import Dash, html, dcc
 import requests
 import os
 
@@ -12,4 +13,38 @@ headers = {
 
 response = requests.request("GET", url, headers=headers)
 
-print(response.text)
+crypto_names = response.text
+print(crypto_names)
+
+# TODO: fix the type to list (str actually)
+print(type(crypto_names))
+
+# For testing only, to be modified later
+
+app = Dash(__name__)
+
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
+
+app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+    html.H1(
+        children='Crypto Dashboard',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+    ),
+
+    html.Div(children=[
+        html.Br(),
+        html.Label('Multi-Select Dropdown'),
+        dcc.Dropdown(options = crypto_names,
+                     value = ['btc', 'eth'],
+                     multi=True),
+    ]),
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
