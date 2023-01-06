12e780e826d561cb9fafbdce3269e74b726cda50
Valentin Henriot <59604857+ValHrt@users.noreply.github.com>
2023-01-06 19:42:39 +0100

    Get selected crypto current price

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 from dash import Dash, html, dcc
+from dash.dependencies import Output, Input
 import requests
 import os
 
 API_KEY = os.environ.get("RAPID_API_KEY")
 
-url = "https://coingecko.p.rapidapi.com/simple/supported_vs_currencies"
+url_vs_currency = "https://coingecko.p.rapidapi.com/simple/supported_vs_currencies"
+url_coins = "https://coingecko.p.rapidapi.com/coins/list"
+url_price = "https://coingecko.p.rapidapi.com/simple/price"
 
 headers = {
 	"X-RapidAPI-Key": API_KEY,
 	"X-RapidAPI-Host": "coingecko.p.rapidapi.com"
 }
 
-response = requests.request("GET", url, headers=headers)
+querystring_price = {"ids":"bitcoin","vs_currencies":"eur"}
 
-crypto_str = response.text
+response_vs_currency = requests.request("GET", url_vs_currency, headers=headers)
+response_coins = requests.request("GET", url_coins, headers=headers)
+response_price = requests.request("GET", url_price, headers=headers,
+                                  params=querystring_price)
 
-crypto_names = eval(crypto_str)
+vs_currency = eval(response_vs_currency.text)
+coins_glob = eval(response_coins.text)
+price = eval(response_price.text)
+
+coins_id = [coin['id'] for coin in coins_glob]
+coins_name = [coin['name'] for coin in coins_glob]
+coin_price = price['bitcoin']['eur']
+
 
 app = Dash(__name__)
-
 
 app.layout = html.Div(children=[
     html.H1(
@@ -27,12 +39,49 @@ app.layout = html.Div(children=[
 
     html.Div(children=[
         html.Br(),
-        html.Label('Crypto-Currencies'),
-        dcc.Dropdown(options = crypto_names,
-                     value = ['btc', 'eth'],
-                     multi=True),
+        html.Label('Target Currency'),
+        dcc.Dropdown(id = "Currency_Selected",
+                     options = vs_currency,
+                     value = 'eur'),
     ]),
+
+    html.Div(children=[
+        html.Br(),
+        html.Label('Cryptocurrency'),
+        dcc.Dropdown(id = "Coin_Selected",
+                     options = coins_name,
+                     value = 'Bitcoin'),
+    ]),
+
+    html.H3(id="current_price", style={'text-align': 'center',
+                                       'color': '#FD841F'})
 ])
+
+
+@app.callback(
+    Output(component_id = "current_price", component_property = "children"),
+    [Input(component_id = "Currency_Selected", component_property = "value"),
+     Input(component_id = "Coin_Selected", component_property = "value")])
+
+
+def get_current_price(currency, coin):
+    title = f"Current Bitcoin price: {coin_price}€"
+    if currency and coin:
+        index = coins_name.index(coin)
+        querystring_price = {"ids": str(coins_glob[index]["id"]),
+                             "vs_currencies":currency}
+        response_price = requests.request("GET", url_price, headers=headers,
+                                  params=querystring_price)
+        price_new = eval(response_price.text)
+        coin_price_new = price_new[str(coins_glob[index]["id"])][currency]
+
+        if currency == "eur":
+            title = f"Current {coin} price: {coin_price_new}€"
+        elif currency == "usd":
+            title = f"Current {coin} price: {coin_price_new}$"
+        else:
+            title = f"Current {coin} price: {coin_price_new} {currency.upper()}"
+    return title
 
 if __name__ == '__main__':
     app.run_server(debug=True)
